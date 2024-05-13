073695fc526a9daf17603020b737f4b317abe4bb
Marcus1618 <mjwentzell@perse.co.uk>
2024-05-13 15:58:55 +0100

    Working terminal UI

--- Ui.py
+++ Ui.py
@@ -1,7 +1,7 @@
 from Game import Game, GameError
 from sys import stderr
 from abc import ABC, abstractmethod
-import PySimpleGUI as sg
+#import PySimpleGUI as sg
 
 class Ui(ABC):
 
@@ -14,7 +14,19 @@ class Terminal(Ui):
         self._game = Game()
 
     def run(self):
-        pass
+        #While there is not winner
+        while self._game.winner is None:
+            print(self._game)
+            try:
+                row = int(input("Enter Row: "))
+                col = int(input("Enter Column: "))
+                self._game.play(row, col)
+            except GameError as e:
+                print(e, file=stderr)
+        print("The winner was", self._game.winner)
+        #Get input from user
+        #Play the input into the game
+
 
 class Gui(Ui):
     pass
